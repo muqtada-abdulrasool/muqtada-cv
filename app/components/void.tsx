ce9a396a5dbf07ed56042d8abf509617d214f2ee
DiiaBlood <DiiaBloodyRain@gmail.com>
2026-08-10 13:41:54 +0300

    Implement Background Void
    The code is still badly unoptimized

---
+++ app/components/void.tsx
@@ -0,0 +1,80 @@
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
+interface VoidProps {
+  pattern: string;
+  speed?: number;
+  size?: number;
+  textureSize?: number;
+  textureAngle?: number;
+}
+
+export function Void({
+  pattern,
+  speed = 1,
+  size = 50,
+  textureSize = 200,
+  textureAngle = 45,
+}: VoidProps) {
+  const leftDivRef = useRef<HTMLDivElement>(null);
+  const rightDivRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!leftDivRef.current || !rightDivRef.current) return;
+
+    const leftLoop = gsap.to(leftDivRef.current, {
+      backgroundPositionX: `-${textureSize}`,
+      ease: "none",
+      duration: (textureSize / 50) * speed,
+      repeat: -1,
+    });
+
+    const rightLoop = gsap.to(rightDivRef.current, {
+      backgroundPositionX: `+${textureSize}`,
+      ease: "none",
+      duration: (textureSize / 50) * speed,
+      repeat: -1,
+    });
+
+    return () => {
+      leftLoop.kill();
+      rightLoop.kill();
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-row w-full h-full bg-black absolute">
+      <div
+        className="relative overflow-hidden h-full bg-purple-800 mask-[linear-gradient(to_right,black_35%,transparent_100%)] perspective-midrange"
+        style={{ minWidth: `${size / 2}%` }}
+      >
+        <div
+          ref={leftDivRef}
+          className="absolute w-[200vw] h-[200vh] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-repeat origin-center"
+          style={{
+            backgroundImage: `url('${pattern}')`,
+            backgroundSize: `${textureSize}px ${textureSize}px`,
+            transform: `rotateY(${textureAngle}deg)`,
+          }}
+        />
+      </div>
+
+      <div className="h-full w-full transparent"></div>
+
+      <div
+        className="relative overflow-hidden h-full  bg-purple-800 mask-[linear-gradient(to_left,black_35%,transparent_100%)] perspective-midrange"
+        style={{ minWidth: `${size / 2}%` }}
+      >
+        <div
+          ref={rightDivRef}
+          className="absolute w-[200vw] h-[200vh] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-repeat origin-center"
+          style={{
+            backgroundImage: `url('${pattern}')`,
+            backgroundSize: `${textureSize}px ${textureSize}px`,
+            transform: `rotateY(-${textureAngle}deg)`,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,9 @@
+import { Void } from "~/components/void";
 import type { Route } from "./+types/home";
-import MemoryCard from "~/components/memory-card";
-import { Image, Text, Texture, View } from "@react-three/drei";
-import ReactableText3D from "~/components/reactable-text-3d";
-import gsap from "gsap";
-import { DEG2RAD } from "three/src/math/MathUtils.js";
+import { View } from "@react-three/drei";
 import RefractorCube from "~/components/refractor-cube";
+import { useEffect, useMemo, useRef } from "react";
 import { setOrbTarget } from "~/contexts/orb-context";
-import { useRef } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,76 +13,26 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const memRef = useRef<HTMLElement>(null);
+  const cubeRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    setOrbTarget(cubeRef.current);
+  });
 
   return (
-    <div className="flex flex-col grow items-center justify-center bg-blue-800">
-      <div className="flex flex-row justify-center items-center">
-        <View className="size-40">
-          <RefractorCube lightIntensity={1} resolution="extreme">
-            <Image url="/icons/ps2.svg" transparent scale={[1.5, 1.5]} />
-          </RefractorCube>
-        </View>
+    <div className="flex flex-col grow items-center justify-center bg-blue-800 relative">
+      {/* Background */}
+      <Void
+        pattern="/patterns/grid-me.png"
+        textureSize={200}
+        textureAngle={45}
+        speed={10}
+        size={50}
+      />
 
-        <div className="flex flex-col items-center">
-          <View className="size-40" ref={memRef}>
-            <MemoryCard scale={0.5} bobHeight={0.5} />
-            <directionalLight position={[0, 0, 5]} intensity={5} />
-          </View>
-
-          <p className="text-xs italic font-bold text-yellow-300">
-            *Don't turn off the website when this icon shows up
-          </p>
-        </div>
-
-        <View
-          className="size-40"
-          ref={(node) => setOrbTarget(node as HTMLElement)}
-        >
-          <RefractorCube hoverable to="/meow">
-            <Text fontSize={0.6}>Meow</Text>
-          </RefractorCube>
-        </View>
-      </div>
-
-      <div className="w-full h-20 ">
-        <ReactableText3D
-          className="w-full h-20"
-          fontSize={1.5}
-          animation={(g) => {
-            gsap.set(g.rotation, { z: -15 * DEG2RAD });
-
-            return gsap
-              .timeline({
-                repeat: -1,
-                repeatDelay: 0.5,
-              })
-              .to(g.rotation, {
-                z: 15 * DEG2RAD,
-                duration: 0.8,
-                ease: "elastic",
-              })
-              .to(
-                g.rotation,
-                {
-                  z: -15 * DEG2RAD,
-                  duration: 0.8,
-                  ease: "elastic",
-                },
-                "+=0.5",
-              );
-          }}
-        >
-          This Site is Still Under Construction
-        </ReactableText3D>
-      </div>
-
-      <button
-        className="w-80 bg-white"
-        onClick={() => setOrbTarget(memRef.current)}
-      >
-        CLICK TO CHANGE TARGET
-      </button>
+      <View className="size-80" ref={cubeRef}>
+        <RefractorCube />
+      </View>
     </div>
   );
 }
