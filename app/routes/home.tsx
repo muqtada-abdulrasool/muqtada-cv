22757d61f7bd19b0197495e94c233ef7fed76071
DiiaBlood <DiiaBloodyRain@gmail.com>
2026-08-11 18:06:17 +0300

    Implemented About Me Section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,6 +4,12 @@ import { View } from "@react-three/drei";
 import RefractorCube from "~/components/refractor-cube";
 import { useEffect, useMemo, useRef } from "react";
 import { setOrbTarget } from "~/contexts/orb-context";
+import Container from "~/components/container";
+import { Avatar, AvatarImage } from "~/components/ui/avatar";
+import ReactableText3D from "~/components/reactable-text-3d";
+import gsap from "gsap";
+import { DEG2RAD } from "three/src/math/MathUtils.js";
+import { Separator } from "~/components/ui/separator";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,14 +19,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const cubeRef = useRef<HTMLElement>(null);
+  const orbRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    setOrbTarget(cubeRef.current);
+    setOrbTarget(orbRef.current);
   });
 
   return (
-    <div className="flex flex-col grow items-center justify-center bg-blue-800 relative">
+    <div className="flex flex-col grow items-center justify-center p-20 gap-20 relative">
       {/* Background */}
       <Void
         pattern="/patterns/grid-me.png"
@@ -30,9 +36,72 @@ export default function Home() {
         size={50}
       />
 
-      <View className="size-80" ref={cubeRef}>
-        <RefractorCube />
-      </View>
+      <Container>
+        {/* Header with PFP, Title, and Job Title */}
+        <div className="flex flex-row gap-10 items-center p-md">
+          <Avatar className="size-60" ref={orbRef}>
+            <AvatarImage src="/Muqtada.jpg" />
+          </Avatar>
+
+          <div className="flex flex-col gap-xs justify-center h-full">
+            <div className="flex flex-col gap-sm">
+              <ReactableText3D
+                charactersFollowMouse
+                FollowMouseRadius={200}
+                fontPath="/fonts/Helvetica/Helvetica-Rounded-Bold.json"
+                fontSize={1.8}
+              >
+                MUQTADA
+              </ReactableText3D>
+              <ReactableText3D
+                charactersFollowMouse
+                FollowMouseRadius={200}
+                fontPath="/fonts/Helvetica/Helvetica-Rounded-Bold.json"
+                fontSize={1.8}
+              >
+                ABDULRASOOL
+              </ReactableText3D>
+            </div>
+
+            <ReactableText3D
+              fontPath="/fonts/Helvetica/Helvetica.json"
+              fontSize={1}
+              color={"yellow"}
+
+              applyAnimationToAllCharacters={false}
+              repeatTriggerCount={1}
+              masterAnimationRepeat={0}
+              animation={(g) =>
+                gsap.timeline({ repeat: 4 }).to(g.rotation, {
+                  y: `+=${Math.PI * 2}`,
+                  duration: 0.1,
+                  ease: "none",
+                })
+              }
+            >
+              *Frontend Developer
+            </ReactableText3D>
+          </div>
+        </div>
+
+        {/* <Separator /> */}
+
+        {/* Bio */}
+        <fieldset className="rounded-lg border border-input p-4">
+          <legend className="-ml-1 px-1 text-md text-foreground">
+            About Me
+          </legend>
+          <div className="text-foreground text-lg">
+            Frontend & Backend Developer and Computer Engineering student with a
+            history in React, TypeScript, Music, & English Writing.
+            <br />
+            <br />
+            Experienced in leading development teams, crafting pleasant
+            interfaces, and delivering creative solutions across diverse
+            projects from game jams to university projects.
+          </div>
+        </fieldset>
+      </Container>
     </div>
   );
 }
